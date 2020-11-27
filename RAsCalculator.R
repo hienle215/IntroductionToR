d04e488507ea5a418465653a2d3a1d46e5f40737
Thi Le (TAU) <hien.le@tuni.fi>
2020-11-27 10:38:21 +0200

    Adding problem introduction

---
+++ RAsCalculator.R
@@ -0,0 +1,49 @@
+###R as calculator
+
+
+1 + 3
+#> [1] 4
+6*(5-1)
+#> [1] 24
+
+# Some useful operators:
+
++: addition
+-: subtraction
+*: multiplication
+/: division
+x %% y: modulus (Remainder from division)
+x %/% y: integer division
+^ or **: exponentiation, e.g. 3^2 or 3**2 to compute 32
+? What is the output of the following code?
+  
+  -1^2 = ?
+  (-1)^2 = ?
+  You can also try out some more complicated (fancier) computations, such as Trigonometric functions (high school nightmare):
+  
+  sin(): sine
+cos(): cosine
+tan(): tangent
+exp(): Exponential with base e
+log(): Logarithm
+log10(): Logarithm with the base 10
+sqrt(): Square root
+abs(): Absolute value
+round(): Round the value
+floor(): Round down the value
+ceiling(): Round up the value
+factorial(): Factorial function
+gamma(): Gamma function
+digamma(): Digamma function
+.
+? What are the solutions to the following equations
+
+sin(??2) = ?
+cos(??) = ?
+tan(??4) = ?
+  ? Can you verify if the function factorial returns the correct result of 5!?
+
+5*4*3*2*1
+#> [1] 120
+factorial(5)
+#> [1] 120
